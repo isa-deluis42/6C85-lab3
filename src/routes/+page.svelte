4776eac0ca42d17898aafcf5f0f78f702786c093
Isabel de Luis <ideluis@mit.edu>
2026-02-25 12:59:52 -0500

    lab 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class = "menu">
+    <ul>
+        <li> <a href = "." class = "current">Home</a></li>
+        <li> <a href = "projects">Projects</a></li>
+        <li> <a href = "contact">Contact</a></li>
+        <li> <a href = "resume"> CV </a></li>
+        <li> <a href = "https://github.com/isa-deluis42" target="_blank">Github</a></li>
+    </ul>
+</nav>
+<h1> Isabel de Luis </h1>
+    <p> I’m a first-year graduate student at MIT AeroAstro, researching with 
+        Prof. Julie Shah’s Interactive Robotics Group and Dr. Ben Armstrong 
+        at the Industrial Performance Center. I received my Bachelor’s of 
+        Science in Engineering from Olin College of Engineering in May 2025.
+        My research is in human-computer interaction, driven by a commitment 
+        to improving the safety and transparency of intelligent systems. 
+        I’ve previously worked on projects at Caltech, MIT, and JPL exploring 
+        human factors and autonomy in aviation and space exploration.</p>
+    <img
+    src="/images/image_me.jpeg"
+    alt = "Isa de Luis with statue of Charles Darwin"/>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,30 @@
-<svelte:head>
-  <title>Contact me</title>
-</svelte:head>
+<nav class = "menu">
+    <ul>
+        <li> <a href = "..">Home</a></li>
+        <li> <a href = "../projects">Projects</a></li>
+        <li> <a href = "." class = "current">Contact</a></li>
+        <li> <a href = "../resume"> CV </a></li>
+        <li> <a href = "https://github.com/isa-deluis42" target="_blank">Github</a></li>
+    </ul>
+</nav>
+<h1>Contact</h1>
+    <form action = "mailto:ideluis@mit.edu"
+    method = "get">
+    
+        <label for="email"> 
+            Enter your email: 
+            <input name="name" type="email"/>
+        </label>
+        
+        <label for="subject"> 
+            Subject: 
+            <input name="subject"/>
+        </label>
+        
+        <label for="main_text"> 
+            Text:
+            <textarea> </textarea>
+        </label>
+        <button>Submit</button>
+        
+    </form>
