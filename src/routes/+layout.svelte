664738cff8a11a3f65e37704f192a2e07524c820
Isabel de Luis <ideluis@mit.edu>
2026-03-03 20:15:45 -0500

    fixed github

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,7 +39,7 @@
         <li> <a href = "resume"> CV </a></li>
         <li> <a href = "https://github.com/isa-deluis42" target="_blank">Github</a></li> -->
         {#each pages as p}
-            <li> <a href = {base + p.url}
+            <li> <a href = {p.url.startsWith("http") ? p.url : base + p.url}
             class:current={p.url === "/" // is this link the home page?
             ? $page.url.pathname === (base + "/") // if yes - set current = true if the path name matches. Else, set current = true if the path name starts correctly
             : $page.url.pathname.startsWith(base + p.url)}
